33d25d41649f4d0900051bc2b89c0e1b3cb34082
titojuan447-blip <titojuan447@gmail.com>
2026-03-20 09:32:28 -0500

    Update BullionVault.js

--- app/plugins/BullionVault.js
+++ app/plugins/BullionVault.js
@@ -4,48 +4,40 @@ var config = require('../../config/config'),
     PriceRequester = require('../models/PriceRequester.js'),
     Streamer = require('../models/Streamer.js');
 
-function BullionVaultPriceRequester(symbol, options) {
+function BinanceGoldPriceRequester(symbol, options) {
     PriceRequester.call(this, symbol, options);
 }
 
-BullionVaultPriceRequester.config = {
-    exchange: 'bullionvault',
+BinanceGoldPriceRequester.config = {
+    exchange: 'bullionvault', // Mantenemos el nombre para que tu API no cambie
     symbol_map: { 
-        "XAUUSD" : "AUXZU", // Símbolo correcto para Oro en USD
-        "XAGUSD" : "AGXZU"  // Símbolo correcto para Plata en USD
+        "XAUUSD" : "PAXGUSDT"
     },
-    // Usamos la URL que devuelve JSON directamente
-    url_template: 'https://www.bullionvault.com/view_market_json.do?currency=USD'
+    url_template: 'https://api.binance.com/api/v3/ticker/bookTicker?symbol=<<SYMBOL>>'
 };
 
-BullionVaultPriceRequester.prototype = Object.create(PriceRequester.prototype);
-BullionVaultPriceRequester.prototype.constructor = BullionVaultPriceRequester;
+BinanceGoldPriceRequester.prototype = Object.create(PriceRequester.prototype);
+BinanceGoldPriceRequester.prototype.constructor = BinanceGoldPriceRequester;
 
-BullionVaultPriceRequester.prototype.processResponse = function (response, body) {
+BinanceGoldPriceRequester.prototype.processResponse = function (response, body) {
     try {
         var data = JSON.parse(body);
-        var symbolToFind = BullionVaultPriceRequester.config.symbol_map[this.symbol];
-        
-        // Buscamos el mercado correcto en la lista de BullionVault
-        var market = data.markets.find(m => m.securityId === symbolToFind);
+        if (data && data.bidPrice) {
+            var bid = parseFloat(data.bidPrice);
+            var ask = parseFloat(data.askPrice);
 
-        if (market && market.pricesBuy && market.pricesBuy.length > 0) {
-            // Precios en BullionVault vienen por KG, convertimos a Onza (32.1507)
-            var bid = market.pricesBuy[0].limit / 32.1507;
-            var ask = market.pricesSell[0].limit / 32.1507;
-
-            console.log("BullionVault JSON OK: " + this.symbol + " -> " + bid);
+            console.log("Binance Gold OK: " + bid);
             return new messages.Symbol(this.getExchange(), this.symbol, bid, ask);
         }
     } catch (e) {
-        console.log("BullionVault Error: " + e.message);
+        console.log("Binance Gold Error: " + e.message);
     }
     return null;
 };
 
 module.exports = {
     register: function () {
-        var BullionVaultStreamer = Streamer(BullionVaultPriceRequester, 20000); // 20 segs
-        Plugin_.register(BullionVaultPriceRequester, BullionVaultStreamer);
+        var BinanceStreamer = Streamer(BinanceGoldPriceRequester, 5000); // Actualiza cada 5 segs
+        Plugin_.register(BinanceGoldPriceRequester, BinanceStreamer);
     }
 };
