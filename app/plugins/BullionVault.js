6328fbb5b398a999c041c8323bc5c293752b925b
titojuan447-blip <titojuan447@gmail.com>
2026-03-19 20:59:02 -0500

    Update BullionVault.js

--- app/plugins/BullionVault.js
+++ app/plugins/BullionVault.js
@@ -31,25 +31,43 @@ BullionVaultPriceRequester.prototype = Object.create(PriceRequester.prototype);
 BullionVaultPriceRequester.prototype.constructor = BullionVaultPriceRequester;
 
 BullionVaultPriceRequester.prototype.processResponse = function (response, body) {
-    var $ = cheerio.load(body),
+    // Forzamos xmlMode para que cheerio lea correctamente las etiquetas del XML de BullionVault
+    var $ = cheerio.load(body, { xmlMode: true }),
         get_price = function(op) {
             var prices = [];
+            
+            // Buscamos los nodos de precio según la operación (buy/sell)
             $(op + "Prices > price").each(function(index, elem){
-                prices.push(elem.attribs.limit);
+                if (elem.attribs && elem.attribs.limit) {
+                    prices.push(parseFloat(elem.attribs.limit));
+                }
             });
-            return Math.min.apply(null, prices) / 32.15;
+
+            if (prices.length === 0) return 0;
+
+            // BullionVault entrega precios por KILOGRAMO. 
+            // Dividimos por 32.1507 para convertir a ONZA TROY.
+            // Para 'buy' buscamos el mejor precio de compra (máximo)
+            // Para 'sell' buscamos el mejor precio de venta (mínimo)
+            var pricePerKilo = (op === "buy" ? Math.max.apply(null, prices) : Math.min.apply(null, prices));
+            return pricePerKilo / 32.1507;
         },
         bid = get_price("buy"),
         ask = get_price("sell");
     
+    // Log de control para ver en Render si el precio se procesó
+    if (bid === 0 || isNaN(bid)) {
+        console.log("BullionVault: Error al extraer precio para " + this.symbol);
+    } else {
+        console.log("BullionVault: Precio actualizado para " + this.symbol + " -> " + bid);
+    }
+    
     return new messages.Symbol(this.getExchange(), this.symbol, bid, ask);
 };
-/**/
 
 module.exports = {
     register: function () {
-        var BullionVaultStreamer = Streamer(BullionVaultPriceRequester,
-                                            15000);
+        var BullionVaultStreamer = Streamer(BullionVaultPriceRequester, 15000);
         Plugin_.register(BullionVaultPriceRequester, BullionVaultStreamer);
     }
-};
+};;
