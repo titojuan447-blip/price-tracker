ededdb31f63ac8002930781e842e9f2a3a1f736f
titojuan447-blip <titojuan447@gmail.com>
2026-03-20 09:22:38 -0500

    Update BullionVault.js

--- app/plugins/BullionVault.js
+++ app/plugins/BullionVault.js
@@ -1,5 +1,4 @@
-var cheerio = require('cheerio'),
-    config = require('../../config/config'),
+var config = require('../../config/config'),
     messages = require('../../public/lib/messages.js'),
     Plugin_ = require('../models/Plugin.js'),
     PriceRequester = require('../models/PriceRequester.js'),
@@ -12,67 +11,41 @@ function BullionVaultPriceRequester(symbol, options) {
 BullionVaultPriceRequester.config = {
     exchange: 'bullionvault',
     symbol_map: { 
-        "XAUUSD" : "GOLD", 
-        "XAGUSD" : "SILVER" 
+        "XAUUSD" : "AUXZU", // Símbolo correcto para Oro en USD
+        "XAGUSD" : "AGXZU"  // Símbolo correcto para Plata en USD
     },
-    url_template: (
-        'https://www.bullionvault.com/view_market_xml.do' +
-        '?considerationCurrency=USD&securityClassNarrative=<<SYMBOL>>'
-    ),
+    // Usamos la URL que devuelve JSON directamente
+    url_template: 'https://www.bullionvault.com/view_market_json.do?currency=USD'
 };
 
 BullionVaultPriceRequester.prototype = Object.create(PriceRequester.prototype);
 BullionVaultPriceRequester.prototype.constructor = BullionVaultPriceRequester;
 
-// Añadimos User-Agent para evitar bloqueos
-BullionVaultPriceRequester.prototype.getOptions = function() {
-    return {
-        headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
-        }
-    };
-};
-
 BullionVaultPriceRequester.prototype.processResponse = function (response, body) {
     try {
-        var $ = cheerio.load(body, { xmlMode: true });
+        var data = JSON.parse(body);
+        var symbolToFind = BullionVaultPriceRequester.config.symbol_map[this.symbol];
         
-        var get_price = function(op) {
-            var prices = [];
-            // BullionVault usa tags como <sellPrices> y <price limit="..."/>
-            var tag = op === "buy" ? "buyPrices" : "sellPrices";
-            
-            $(tag + " price").each(function(i, elem) {
-                if (elem.attribs && elem.attribs.limit) {
-                    prices.push(parseFloat(elem.attribs.limit));
-                }
-            });
+        // Buscamos el mercado correcto en la lista de BullionVault
+        var market = data.markets.find(m => m.securityId === symbolToFind);
 
-            if (prices.length === 0) return 0;
+        if (market && market.pricesBuy && market.pricesBuy.length > 0) {
+            // Precios en BullionVault vienen por KG, convertimos a Onza (32.1507)
+            var bid = market.pricesBuy[0].limit / 32.1507;
+            var ask = market.pricesSell[0].limit / 32.1507;
 
-            // Oro: precio por Kilo / 32.1507 = Onza
-            var val = (op === "buy" ? Math.max(...prices) : Math.min(...prices));
-            return val / 32.1507;
-        };
-
-        var bid = get_price("buy");
-        var ask = get_price("sell");
-
-        if (bid > 0) {
-            console.log("BullionVault OK: " + this.symbol + " -> " + bid);
+            console.log("BullionVault JSON OK: " + this.symbol + " -> " + bid);
             return new messages.Symbol(this.getExchange(), this.symbol, bid, ask);
-        } else {
-            console.log("BullionVault: XML recibido pero sin precios para " + this.symbol);
         }
     } catch (e) {
-        console.log("BullionVault: Error procesando XML: " + e.message);
+        console.log("BullionVault Error: " + e.message);
     }
     return null;
 };
 
 module.exports = {
     register: function () {
-        var BullionVaultStreamer = Streamer(BullionVaultPriceRequester, 15000);
+        var BullionVaultStreamer = Streamer(BullionVaultPriceRequester, 20000); // 20 segs
         Plugin_.register(BullionVaultPriceRequester, BullionVaultStreamer);
     }
 };
